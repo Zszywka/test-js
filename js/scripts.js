3f4d20ceae2ca8e49a144054168c0a747b547997
Joanna <piotrowskaWIMIIP@gmail.com>
2018-04-01 21:29:03 +0200

    Add coments in scripts.js

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+// My first code in JS
+var name = prompt('Enter your name');
+// alert('Hello, ' + name);
+console.log('Hello, ' + name);
+
+/*
+  $(document).ready(function() {
+    $('header').hide().slideDown(3000);
+  });
+*/
